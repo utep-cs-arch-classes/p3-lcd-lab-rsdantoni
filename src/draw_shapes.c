7311ddf474c9a1637f862b188831f5bb7db7567b
rsdantoni <rsdantoni@miners.utep.edu>
2022-04-26 16:09:53 -0600

    Changed variables into default values, modified values

--- src/draw_shapes.c
+++ src/draw_shapes.c
@@ -1,16 +1,16 @@
 #include "lcdutils.h"
 #include "lcddraw.h"
 
+    int unit_height = 5, unit_width = 5;
+    int center_row = screenWidth/2, center_col = screenWidth / 2;
+    int left_col = col - (width / 2);
+    int top_row  = row - (height / 2);
 
 void draw_turtle(){
     
     u_int shellColor = COLOR_DARK_GREEN;
-    int height = 5;
-    int width = 10;
-    int row = 20, col = screenWidth / 2;
-    int left_col = col - (width / 2);
-    int top_row  = row - (height / 2);
+    
 
     fillRectangle(left_col, top_row, width, height, shellColor);
-    
+    fillRectangle(    
 }
