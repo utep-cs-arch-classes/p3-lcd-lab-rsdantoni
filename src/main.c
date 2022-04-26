7298e5eb243e05757f808537e629b2366956530d
Roberto <rsdantoni@miners.utep.edu>
2022-04-26 13:05:04 -0600

    Creation of main file

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <msp430.h>
+#include <libTimer.h>
+#include "lcdutils.h"
+#include "lcddraw.h"
+#include "draw_shapes.h"
+
+// WARNING: LCD DISPLAY USES P1.0.  Do not touch!!! 
+
+void main()
+{
+  configureClocks();
+  lcd_init();
+
+  // clear the screen in case something is already on there
+  clearScreen(COLOR_WHITE);
+
+  draw_turtle();
+}
