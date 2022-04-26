b7c2c43593030946bb12394b3d5d3b84a074e77c
Roberto <rsdantoni@miners.utep.edu>
2022-04-26 16:14:32 -0600

    Corrected variable names and scope

--- src/draw_shapes.c
+++ src/draw_shapes.c
@@ -3,14 +3,14 @@
 
     int unit_height = 5, unit_width = 5;
     int center_row = screenWidth/2, center_col = screenWidth / 2;
-    int left_col = col - (width / 2);
-    int top_row  = row - (height / 2);
+    
 
 void draw_turtle(){
     
     u_int shellColor = COLOR_DARK_GREEN;
     
-
-    fillRectangle(left_col, top_row, width, height, shellColor);
-    fillRectangle(    
+    int left_col = center_col - (unit_width / 2);
+    int top_row  = center_row - (unit_height / 2);
+    
+    fillRectangle(left_col, top_row, unit_width * 2, unit_height, shellColor);
 }
