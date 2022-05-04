ff72034e2e34d9403934ed2e442a5d4bbfcd964b
rsdantoni <rsdantoni@miners.utep.edu>
2022-05-04 02:16:44 -0600

    Enable switch state checking

--- src/wdtInterruptHandler.c
+++ src/wdtInterruptHandler.c
@@ -1,13 +1,17 @@
 #include <msp430.h>
 #include "libTimer.h"
+#include "lcdutils.h"
+#include "lcddraw.h"
 #include "draw_shapes.h"
+#include "switches.h"
 
 void __interrupt_vec(WDT_VECTOR) WDT(){
   const  unsigned int second_limit = 50;
   static unsigned int second_count = 0;
 
+  check_p2_switches();
   second_count++;
-
+  
   if (second_count >= second_limit) {
     move_shapes();
     second_count = 0;
