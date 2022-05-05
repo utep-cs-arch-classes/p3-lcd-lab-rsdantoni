ceca5908ec382551e5ad985017f8f657487350c0
Roberto <rsdantoni@miners.utep.edu>
2022-05-04 19:03:42 -0600

    Created new shape, code cleanup

--- src/draw_shapes.c
+++ src/draw_shapes.c
@@ -14,68 +14,68 @@ donut don;
 int turt_vel_x = 0, turt_vel_y = 0;
 int donut_vel_x = 6, donut_vel_y = 6;
 
-void draw_right_turtle(int x, int y, bool erase){
-   
-    u_int shell_color, flesh_color, eye_color;
+void init_shapes(){
+  turt.turt_row = turtle_x;
+  turt.turt_col = turtle_y;
+  turt.old_turt_row = turtle_x;
+  turt.old_turt_col = turtle_y;
 
-    if(erase){
-      shell_color = COLOR_WHITE;
-      flesh_color = COLOR_WHITE;
-      eye_color = COLOR_WHITE;
-    } else{
-      shell_color = COLOR_DARK_GREEN;
-      flesh_color = COLOR_TAN;
-      eye_color = COLOR_BLACK;
-    }
-    
-    int left_col = x - (6 * unit_width / 2);
-    int top_row  =  y;
-    top_row -= (3 * unit_height / 2);
-    fillRectangle(left_col, top_row, unit_width * 6, unit_height * 3, shell_color);
-
-    left_col = x - (4 * unit_width / 2); 
-    top_row -= unit_height;
-    fillRectangle(left_col, top_row, unit_width * 4, unit_height, shell_color);
-
-    left_col = x - (2 * unit_width / 2);
-    top_row -= unit_height;
-    fillRectangle(left_col, top_row, 2 * unit_width, unit_height, shell_color);
-
-    left_col = x - (6 * unit_height / 2);
-    top_row += unit_height * 5;
-    fillRectangle(left_col, top_row, 2 * unit_width, 2 * unit_height, flesh_color);
-
-    left_col = x - (2 * unit_width / 2);
-    fillRectangle(left_col, top_row, 2 * unit_width, unit_height, flesh_color);
-
-    left_col = x + (2 * unit_width / 2);
-    fillRectangle(left_col, top_row, 2 * unit_width, 2 * unit_height, flesh_color);
-
-    left_col = x + (6 * unit_width / 2);
-    top_row -= 4 * unit_height;
-    fillRectangle(left_col, top_row, 2 * unit_width, 4 * unit_height, flesh_color);
-
-    left_col = x + (8 * unit_width / 2);
-    top_row += unit_height;
-    fillRectangle(left_col, top_row, unit_width, unit_height, eye_color);
-  
+  don.donut_row = donut_x;
+  don.donut_col = donut_y;
+  don.old_donut_row = donut_x;
+  don.old_donut_col = donut_y;
 }
 
 void move_shapes(){
-  moveDonut(&don);
+  move_donut(&don);
   move_turtle(&turt);
 }
 
-void init_shapes(){
-    turt.turt_row = turtle_x;
-    turt.turt_col = turtle_y;
-    turt.old_turt_row = turtle_x;
-    turt.old_turt_col = turtle_y;
+void draw_right_turtle(int x, int y, bool erase){
+   
+  u_int shell_color, flesh_color, eye_color;
 
-    don.donut_row = donut_x;
-    don.donut_col = donut_y;
-    don.old_donut_row = donut_x;
-    don.old_donut_col = donut_y;
+  if(erase){
+    shell_color = COLOR_WHITE;
+    flesh_color = COLOR_WHITE;
+    eye_color = COLOR_WHITE;
+  } else{
+    shell_color = COLOR_DARK_GREEN;
+    flesh_color = COLOR_TAN;
+    eye_color = COLOR_BLACK;
+  }
+  
+  int left_col = x - (6 * unit_width / 2);
+  int top_row  =  y;
+  top_row -= (3 * unit_height / 2);
+  fillRectangle(left_col, top_row, unit_width * 6, unit_height * 3, shell_color);
+
+  left_col = x - (4 * unit_width / 2); 
+  top_row -= unit_height;
+  fillRectangle(left_col, top_row, unit_width * 4, unit_height, shell_color);
+
+  left_col = x - (2 * unit_width / 2);
+  top_row -= unit_height;
+  fillRectangle(left_col, top_row, 2 * unit_width, unit_height, shell_color);
+
+  left_col = x - (6 * unit_height / 2);
+  top_row += unit_height * 5;
+  fillRectangle(left_col, top_row, 2 * unit_width, 2 * unit_height, flesh_color);
+
+  left_col = x - (2 * unit_width / 2);
+  fillRectangle(left_col, top_row, 2 * unit_width, unit_height, flesh_color);
+
+  left_col = x + (2 * unit_width / 2);
+  fillRectangle(left_col, top_row, 2 * unit_width, 2 * unit_height, flesh_color);
+
+  left_col = x + (6 * unit_width / 2);
+  top_row -= 4 * unit_height;
+  fillRectangle(left_col, top_row, 2 * unit_width, 4 * unit_height, flesh_color);
+
+  left_col = x + (8 * unit_width / 2);
+  top_row += unit_height;
+  fillRectangle(left_col, top_row, unit_width, unit_height, eye_color);
+
 }
 
 void move_turtle(turtle *to_draw){
@@ -96,46 +96,46 @@ void move_turtle(turtle *to_draw){
     turt_vel_y = -5;
   }
 
-  // if(turt_vel_x >= 0){
-    draw_right_turtle(to_draw->old_turt_row, to_draw->old_turt_col, true);
-    draw_right_turtle(to_draw->turt_row, to_draw->turt_col, false);
-      // }
+ // if(turt_vel_x >= 0){
+  draw_right_turtle(to_draw->old_turt_row, to_draw->old_turt_col, true);
+  draw_right_turtle(to_draw->turt_row, to_draw->turt_col, false);
+    // }
 
-    to_draw-> old_turt_row = to_draw->turt_row;
-    to_draw->old_turt_col = to_draw->turt_col;
+  to_draw-> old_turt_row = to_draw->turt_row;
+  to_draw->old_turt_col = to_draw->turt_col;
 
-    to_draw->turt_row += turt_vel_x;
-    to_draw->turt_col += turt_vel_y;
-    
-    if( (to_draw->turt_row + 4.5) >= screenWidth ){
-        to_draw->turt_row = 5;
-    }
+  to_draw->turt_row += turt_vel_x;
+  to_draw->turt_col += turt_vel_y;
+  
+  if( (to_draw->turt_row + 4.5) >= screenWidth ){
+      to_draw->turt_row = 5;
+  }
 
-    if( (to_draw->turt_row - 4.5) <= 0 ){
-        to_draw->turt_row = screenWidth - 4.5;
-    }
+  if( (to_draw->turt_row - 4.5) <= 0 ){
+      to_draw->turt_row = screenWidth - 4.5;
+  }
 }
 
-void moveDonut(donut *to_draw){
-   drawDonut(to_draw->old_donut_row, to_draw->old_donut_col, true);
-   drawDonut(to_draw->donut_row, to_draw->donut_col, false);
+void move_donut(donut *to_draw){
+  draw_donut(to_draw->old_donut_row, to_draw->old_donut_col, true);
+  draw_donut(to_draw->donut_row, to_draw->donut_col, false);
 
-   to_draw->old_donut_row = to_draw->donut_row;
-   to_draw->old_donut_col = to_draw->donut_col;
+  to_draw->old_donut_row = to_draw->donut_row;
+  to_draw->old_donut_col = to_draw->donut_col;
 
-   to_draw->donut_row += donut_vel_x;
-   to_draw->donut_col += donut_vel_y;
+  to_draw->donut_row += donut_vel_x;
+  to_draw->donut_col += donut_vel_y;
 
-   if (to_draw->donut_col + 20 >= screenHeight || (to_draw->donut_col - 20) <= 0){
-     donut_vel_y *= -1;
-   }
+  if (to_draw->donut_col + 20 >= screenHeight || (to_draw->donut_col - 20) <= 0){
+    donut_vel_y *= -1;
+  }
 
-   if ( (to_draw->donut_row + 20) >= screenWidth || (to_draw->donut_row - 20) <= 0){
-     donut_vel_x *= -1;
-   }
+  if ( (to_draw->donut_row + 20) >= screenWidth || (to_draw->donut_row - 20) <= 0){
+    donut_vel_x *= -1;
+  }
 }
 
-void drawDonut(int x_coord, int y_coord, bool erase){
+void draw_donut(int x_coord, int y_coord, bool erase){
   
   u_int frosting_color, bread_color;
 
@@ -152,8 +152,26 @@ void drawDonut(int x_coord, int y_coord, bool erase){
   draw_circle(x_coord, y_coord, 10, COLOR_WHITE);
 }
 
+void draw_barrel(int x, int y, bool erase){
+  u_int barrel_color, warning_color;
 
-void drawHorizontalLine(u_int x_start, u_int x_end, u_int y, u_int colorBGR) {
+  if(erase){
+    barrel_color = COLOR_WHITE;
+    warning_color = COLOR_YELLOW;
+  }else{
+    barrel_color = COLOR_BLACK;
+    barrel_color = COLOR_YELLOW;
+  }
+
+  int top_row = 7 * unit_height / 2;
+  int left_col = 4 * unit_height / 2;
+
+  fillRectangle(left_col, top_row, 7 * unit_width, 4 * unit_height, barrel_color);
+  fillCircle(x, y, 1, warning_color);
+}
+
+
+void draw_horizontal_line(u_int x_start, u_int x_end, u_int y, u_int colorBGR) {
   u_int length = x_end - x_start;
   // set the draw area from the start of the line to the end
   // height is 1 since its a line
@@ -164,14 +182,14 @@ void drawHorizontalLine(u_int x_start, u_int x_end, u_int y, u_int colorBGR) {
   
 }
 
-void drawLines(u_int x_coord, u_int y_coord, u_int x_point, u_int y_point, u_int color) {
+void draw_lines(u_int x_coord, u_int y_coord, u_int x_point, u_int y_point, u_int color) {
   // bottom
-  drawHorizontalLine(x_coord - x_point, x_coord + x_point, y_coord + y_point, color);
+  draw_horizontal_line(x_coord - x_point, x_coord + x_point, y_coord + y_point, color);
   // top
-  drawHorizontalLine(x_coord - x_point, x_coord + x_point, y_coord - y_point, color);
+  draw_horizontal_line(x_coord - x_point, x_coord + x_point, y_coord - y_point, color);
   // and the middle two
-  drawHorizontalLine(x_coord - y_point, x_coord + y_point, y_coord + x_point, color);
-  drawHorizontalLine(x_coord - y_point, x_coord + y_point, y_coord - x_point, color);
+  draw_horizontal_line(x_coord - y_point, x_coord + y_point, y_coord + x_point, color);
+  draw_horizontal_line(x_coord - y_point, x_coord + y_point, y_coord - x_point, color);
 					  
 }
 
@@ -180,7 +198,7 @@ void draw_circle(int x_coord, int y_coord, int r, u_int color) {
   int y_point = r;
   int decision = 3 - (2 * r);
 
-  drawLines(x_coord, y_coord, x_point, y_point, color);
+  draw_lines(x_coord, y_coord, x_point, y_point, color);
 
   while (y_point >= x_point) {
     // move x over one
@@ -194,6 +212,6 @@ void draw_circle(int x_coord, int y_coord, int r, u_int color) {
     } else {
       decision = decision + 4 * x_point + 6;
     }
-    drawLines(x_coord, y_coord, x_point, y_point, color);
+    draw_lines(x_coord, y_coord, x_point, y_point, color);
   }
 }
--- src/draw_shapes.h
+++ src/draw_shapes.h
@@ -16,17 +16,29 @@ typedef struct donut_st {
   int old_donut_col;
 } donut;
 
+typedef struct barrel_st {
+  int barrel_row;
+  int_barrel_col;
+  int old_barrel_row;
+  int old_barrel_col;
+}
+
+
+void init_shapes();
 void move_shapes();
 
 void draw_right_turtle(int x, int y, bool erase);
 void move_turtle(turtle *to_draw);
 
-void init_shapes();
 
-void drawDonut(int x_coord, int y_coord, bool erase);
-void moveDonut(donut *to_draw);
-void drawHorizontalLine(u_int x_start, u_int x_end, u_int y, u_int colorBGR);
-void drawLines(u_int x_coord, u_int y_coord, u_int x_point, u_int y_point, u_int color);
+void draw_donut(int x_coord, int y_coord, bool erase);
+void move_donut(donut *to_draw);
+
+void draw_barrel(int x, int y, bool erase);
+void move_barrel(barrel *to_draw);
+
+void draw_horizontal_line(u_int x_start, u_int x_end, u_int y, u_int colorBGR);
+void draw_lines(u_int x_coord, u_int y_coord, u_int x_point, u_int y_point, u_int color);
 void draw_circle(int x_coord, int y_coord, int r, u_int color);
 
 #endif // _DRAW_SHAPES_H_
